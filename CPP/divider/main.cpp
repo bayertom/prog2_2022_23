27875220dc090d794f7d36fe541d7bae5d19985b
bayertom <bayertom@natur.cuni.cz>
2023-05-10 21:43:06 +0200

    ok

--- CPP/divider/main.cpp
+++ CPP/divider/main.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 
-bool isDevided(int a, int c)
+bool isDivided(int a, int c)
 {
     //Is a divided by c?
     return a%c == 0;
 }
 
-bool areDevided(int a, int b, int c)
+bool areDivided(int a, int b, int c)
 {
-    //Is a divided by c?
+    //Are a, b divided by c?
     bool resa = (a%c == 0);
     bool resb = (b%c == 0);
 
@@ -28,7 +28,7 @@ std::vector<int> getDividers(int a)
     for (int c = 1; c <= a/2; c++)
     {
         //Is a divided by c?
-        bool result = isDevided(a, c);
+        bool result = isDivided(a, c);
 
         //Number a is divided by c
         if (result)
@@ -43,14 +43,13 @@ std::vector<int> getDividers(int a)
 std::vector<int> getDividers2(int a, int b)
 {
     //Get all common dividers of a, b
-    //Store all dividers of a
     std::vector<int> dividers;
 
     //Test all reasonable dividers
     for (int c = 1; c <= a/2; c++)
     {
         //Are a, b divided by c
-        bool result = areDevided(a, b, c);
+        bool result = areDivided(a, b, c);
 
         //Both numbers are divided by c
         if (result)
@@ -61,6 +60,7 @@ std::vector<int> getDividers2(int a, int b)
 
     return dividers;
 }
+
 
 void printVector(std::vector<int> &v)
 {
@@ -74,14 +74,14 @@ void printVector(std::vector<int> &v)
 
 int main()
 {
-    //Number and its divider
+    //Numbers and their dividers
     int a = 24;
     int b = 48;
     int c = 5;
 
     //Test dividers
-    bool result = isDevided(a, c);
-    bool result2 = areDevided(a, b, c);
+    bool result = isDivided(a, c);
+    bool result2 = areDivided(a, b, c);
 
     //Print results
     std::cout << "a divided by c: " << result << '\n';
@@ -94,5 +94,4 @@ int main()
     //Print results
     printVector(dividers);
     printVector(dividers2);
-
 }
